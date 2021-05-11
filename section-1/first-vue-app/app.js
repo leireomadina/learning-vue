cd6029a8ef861b6dee9c17b3d3811c45e792774f
Leire Ordeñana <leireomadina@gmail.com>
2021-05-11 17:50:33 +0200

    Add first vue app

---
+++ section-1/first-vue-app/app.js
@@ -0,0 +1,34 @@
+// VUE
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
+
+// VANILLA JAVASCRIPT: same funcionality 
+
+/* 
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+function addGoal() {
+  const enteredValue = inputEl.value;
+  const listItemEl = document.createElement('li');
+  listItemEl.textContent = enteredValue;
+  listEl.appendChild(listItemEl);
+  inputEl.value= '';
+}
+
+buttonEl.addEventListener('click', addGoal);
+*/
