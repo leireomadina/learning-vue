dc436c8093d857d427ee54a064e2327059d34c3a
Leire Ordeñana <leireomadina@gmail.com>
2021-05-13 19:40:32 +0200

    Add event modifiers

--- section-2/03-counter-app/app.js
+++ section-2/03-counter-app/app.js
@@ -1,22 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: ''
+      counter: 10,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
     add(num) {
-      this.counter = this.counter + num;
+      this.counter += num;
     },
     reduce(num) {
-      this.counter = this.counter - num;
+      this.counter -= num;
     },
     // setName(event) {
     //   this.name = event.target.value;
     // },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName;
+    },
+    submitForm(event) {
+      event.preventDefault();
+      alert('submitted!');
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
     }
   }
 });
