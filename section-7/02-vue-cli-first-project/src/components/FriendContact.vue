53b79378af0667f1f036032ed13396333fdbd99d
Leire Ordeñana <leireomadina@gmail.com>
2021-05-30 15:53:46 +0200

    Add first vue cli project base

---
+++ section-7/02-vue-cli-first-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+<section>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '1203 4596 23',
+        email: 'manuel@localhost.com'
+      }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
+
