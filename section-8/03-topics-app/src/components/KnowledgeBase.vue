8f631568ef756c321d8f8d8e5479af68e746cd3d
Leire Ordeñana <leireomadina@gmail.com>
2021-06-09 11:31:02 +0200

    Add provide and inject exercise

---
+++ section-8/03-topics-app/src/components/KnowledgeBase.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <knowledge-grid :topics="topics"></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['topics'],
+};
+</script>
