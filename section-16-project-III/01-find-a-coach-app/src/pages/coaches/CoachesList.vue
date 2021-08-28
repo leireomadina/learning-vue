1280a618ff9c319b04c361eb6da786d201be01d0
Leire Ordeñana <leireomadina@gmail.com>
2021-08-28 13:39:08 +0200

    Add coach registration functionality and frontend form validation

--- section-16-project-III/01-find-a-coach-app/src/pages/coaches/CoachesList.vue
+++ section-16-project-III/01-find-a-coach-app/src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register">Register as Coach</base-button>
+        <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -60,7 +60,10 @@ export default {
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
-    }
+    },
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
+    } 
   },
   methods: {
     setFilters(updatedFilters) {
