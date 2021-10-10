67d8f11a111fc0c832935883ec7274131ed1c3ee
Leire Ordeñana <leireomadina@gmail.com>
2021-10-10 13:08:52 +0200

    Solve composition api exercise 1

--- section-19-composition-api/02-composition-assignment-1/src/App.vue
+++ section-19-composition-api/02-composition-assignment-1/src/App.vue
@@ -2,9 +2,10 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="goal.isGoalVisible">{{ goal.text }}</h3>
+  <!-- <h3 v-if="isGoalVisible">{{ goal }}</h3> -->
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +14,40 @@
 </template>
 
 <script>
+// import { ref } from 'vue';
+import { reactive } from 'vue';
+
 export default {
-  
-}
+  setup() {
+    // config 1: refs
+    /*
+    const goal = ref('Learn Vue');
+    let isGoalVisible = ref(true);
+    function toggleGoal() {
+      isGoalVisible.value = !isGoalVisible.value;
+    }
+
+    return {
+      goal, isGoalVisible, toggleGoal
+    };
+    */
+
+    // config 2: reactive object
+    const goal = reactive({
+      text: 'Lear Vue',
+      isGoalVisible: true
+    });
+
+    function toggleGoal() {
+      goal.isGoalVisible = !goal.isGoalVisible;
+    }
+
+    return {
+      goal,
+      toggleGoal
+    }
+  }
+};
 </script>
 
 <style>
@@ -25,5 +57,8 @@ html {
 body {
   margin: 3rem;
   text-align: center;
+}
+.active {
+  display: none;
 }
 </style>
