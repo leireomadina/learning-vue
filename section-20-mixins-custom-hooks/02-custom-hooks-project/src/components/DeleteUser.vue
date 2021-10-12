cb405395e339287db61e4fb67ddc06fb7f5cbc71
Leire Ordeñana <leireomadina@gmail.com>
2021-10-12 12:29:17 +0200

    Add a custom hook to display an alert

--- section-20-mixins-custom-hooks/02-custom-hooks-project/src/components/DeleteUser.vue
+++ section-20-mixins-custom-hooks/02-custom-hooks-project/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -11,26 +11,23 @@
 <script>
 import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert.js';
 
 export default {
   components: {
     UserAlert,
   },
   setup() {
-    const alertIsVisible = ref(false);
-
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-
+    const alertTitle = ref('Delete user?');
+    // const alertArr = useAlert();
+    const [ alertIsVisible, showAlert, hideAlert ] = useAlert(true);
+    
     return {
       alertIsVisible,
       showAlert,
       hideAlert,
-    };
+      alertTitle
+    }
   },
 };
 </script>
