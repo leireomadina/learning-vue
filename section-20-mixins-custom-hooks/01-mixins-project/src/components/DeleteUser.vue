d8da1dc823940e82580b6c0621b163f09052865f
Leire Ordeñana <leireomadina@gmail.com>
2021-10-12 12:03:27 +0200

    Add local and global mixins with the Options API

--- section-20-mixins-custom-hooks/01-mixins-project/src/components/DeleteUser.vue
+++ section-20-mixins-custom-hooks/01-mixins-project/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -9,24 +9,15 @@
 </template>
 
 <script>
-import UserAlert from './UserAlert.vue';
+// import UserAlert from './UserAlert.vue';
+import alertMixin from '../mixins/alert.js';
 
 export default {
-  components: {
-    UserAlert,
-  },
   data() {
     return {
-      alertIsVisible: false,
-    };
+      alertTitle: 'Delete user?'
+    }
   },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
-  },
+  mixins: [ alertMixin ]
 };
 </script>
